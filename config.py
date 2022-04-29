bb09a446da181f8274acf94be97fef07f71ffe96
udonbaka <udonbaka@gmail.com>
2022-04-29 09:13:37 +0900

    This is my first commit.

---
+++ config.py
@@ -0,0 +1,26 @@
+# エラー判定フラグ
+error_flg = False
+
+# ChromeDriverのパス
+CHROMEDRIVER = "/Users/hideki/Documents/jupyter/HotelPriceCollector/chromedriver"
+ 
+# チェックイン・アウト日付
+checkin_date = "2022/04/27"
+checkout_date = "2022/04/28"
+
+# アクセスするURL
+## コンフォートイン六本木
+target_url_1 = "https://www.choice-hotels.jp/inn/roppongi/"
+hotel_name_1 = "コンフォートイン六本木"
+
+## アクトホテル六本木
+target_url_2 = "https://www.act-hotel.com/roppongi/"
+hotel_name_2 = "アクトホテル六本木"
+
+## アパホテル六本木駅前
+target_url_3 = "https://www.apahotel.com/hotel/syutoken/tokyo/roppongi-ekimae/"
+hotel_name_3 = "アパホテル六本木駅前"
+
+## エージェントテスト
+target_url_4 = "http://publicgoods.biz/temp/getAgent.php"
+hotel_name_4 = "テストサーバ"
